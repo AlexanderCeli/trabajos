597c0ccd2b1df7de4b11f6619b8173bbf37f4d2c
Alexander <Alexander@Alexander>
2020-01-08 16:10:16 -0500

    JOtion

--- src/Joption/email.java
+++ src/Joption/email.java
@@ -7,7 +7,7 @@ import javax.swing.JOptionPane;
 
 public class email {
 
-      public void verficar() {
+    public void verficar() {
 
         boolean retorno = false;
         int validacion = 0;
@@ -16,7 +16,7 @@ public class email {
         String valida1 = email, valida2 = email;
 
         int contador1 = 0, valor = 3, conta2 = 0;
-       
+
         for (int i = 0; i < valida1.length(); i++) {
 
             contador1++;
@@ -25,32 +25,31 @@ public class email {
             validacion = 1;
         }
         if (validacion == 0) {
-            JOptionPane.showMessageDialog(null, "Debe contener mas tres caracteres   " );
+            JOptionPane.showMessageDialog(null, "Debe contener mas tres caracteres   ");
             retorno = false;
         } else {
             if (contador1 < valor) {
                 validacion = 0;
             }
         }
-        
+
         boolean arroba = false;
         boolean punto = false;
         char arro = '@';
         char pnt = '.';
 
-        
         for (int i = 0; i < email.length(); i++) {
             if (email.charAt(i) == arro && email.charAt(i) == pnt) {
                 arroba = true;
                 punto = true;
             }
         }
-        
-           if (email.matches("..+@@.+\\...+")) {
-            JOptionPane.showMessageDialog(null,"El mail ingresado es incorrecto");
-            }else{
-          JOptionPane.showMessageDialog(null,"El mail ingresado es correcto" );
-           }
-            }
+
+        if (email.matches("..+@@.+\\...+")) {
+            JOptionPane.showMessageDialog(null, "El mail ingresado es incorrecto");
+        } else {
+            JOptionPane.showMessageDialog(null, "El mail ingresado es correcto");
+        }
+    }
 
 }
